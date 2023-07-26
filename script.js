02afedb1a8a23984c9859fddda07df9e2610f351
Ahmed M <devahmedm7@gmail.com>
2023-07-26 12:00:39 +0500

    Fixes some bugs which arose after adding Styles

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-let dateInput = document.querySelector('#datetime');
-let numberOfMonths = document.querySelector('#monthselector');
-let submitButton = document.querySelector('#submitbutton');
-let timeContainer = document.querySelector('#displaydays');
-let hoursContainer = document.querySelector('#displayhours');
-let minutesContainer = document.querySelector('#displayminutes');
-let secondsContainer = document.querySelector('#displayseconds');
-let resetButton = document.querySelector('#reset');
+const dateInput = document.querySelector('#datetime');
+const numberOfMonths = document.querySelector('#monthselector');
+const submitButton = document.querySelector('#submitbutton');
+const timeContainer = document.querySelector('#timecontainer');
+const daysContainer = document.querySelector('#displaydays');
+const hoursContainer = document.querySelector('#displayhours');
+const minutesContainer = document.querySelector('#displayminutes');
+const secondsContainer = document.querySelector('#displayseconds');
+const resetButton = document.querySelector('#reset');
 let monthsValue;
 
 
-let endingDate = [];
+const endingDate = [];
 function getFromLocalStorage() {
     let  endDateValue= localStorage.getItem('endDateValue');
     if (endDateValue) {
@@ -34,17 +35,17 @@ function getFromLocalStorage() {
     finalDate.setHours(finalDate.getHours() + 12);
     finalDate = finalDate.getTime();
     let currentDate = new Date().getTime();
-    let calculateDiff = finalDate - currentDate
+    let calculateDiff = finalDate - currentDate;
     let daysRemaining = Math.floor((calculateDiff / (1000*60*60*24)));
     let hoursRemaining = Math.floor((calculateDiff % (1000* 60 * 60 * 24) / (1000 *60 * 60)));
     let minutesRemaining = Math.floor((calculateDiff % (1000 * 60 * 60) /(1000 * 60)) );
     let secondsRemaining = Math.floor((calculateDiff % (1000 * 60) / 1000 ));
-    timeContainer.innerHTML = daysRemaining + " Days ";
-    hoursContainer.innerHTML = ` ${hoursRemaining}   Hours `;
-    minutesContainer.innerHTML=  minutesRemaining + " Minutes ";  
-    secondsContainer.innerHTML = secondsRemaining + " Seconds";
+    daysContainer.textContent = `${daysRemaining} Days  `;
+    hoursContainer.textContent = `  ${hoursRemaining} Hours`;
+    minutesContainer.textContent= `  ${minutesRemaining} Minutes`;  
+    secondsContainer.textContent = `  ${secondsRemaining} Seconds`;
     if (calculateDiff < 0){
-        timeContainer.innerHTML = "Your staking period has ended, please go to the staking website and claim your tokens or restake!"
+        timeContainer.textContent = "Your staking period has ended, please go to the staking website and claim your tokens or restake!"
         clearInterval(countdownInterval)
     }
 }}
@@ -57,19 +58,23 @@ function calculateTime(event){
    event.preventDefault()
    clearInterval(countdownInterval)
    let dateValue = parseInt(dateInput.value);
-   monthsValue = parseInt(numberOfMonths.value);    
+   monthsValue = parseInt(numberOfMonths.value);
+   console.log(monthsValue)    
    if ((monthsValue === 1 || monthsValue === 3 || monthsValue === 6 || monthsValue === 12) && dateValue > 0){
         endingDate[0] = monthsValue;
         endingDate[1] = dateInput.value
         addToLocalStorage() 
         numberOfMonths.value = '' 
         showCountDown()
+        console.log(endingDate)
 
 
 }  else {
     numberOfMonths.value  = ""
-    timeContainer.innerHTML = "Please select valid number for months (1, 3, 6 or 12) and a Valid Date"
-
+   timeContainer.textContent=  "Please select valid number for months (1, 3, 6 or 12) and a Valid Date";
+    setTimeout( function(){
+        location.reload()
+    }, 3000)
 }
 
 
@@ -85,11 +90,14 @@ function showCountDown(){
     endDate.setHours(endDate.getHours() + 12);
     endDate = endDate.getTime();
     let targetDate = endDate - initialDate;  
-     let daysRemaining = Math.floor((targetDate / (1000*60*60*24)));
+    let daysRemaining = Math.floor((targetDate / (1000*60*60*24)));
     let hoursRemaining = Math.floor((targetDate % (1000* 60 * 60 * 24) / (1000 *60 * 60)));
     let minutesRemaining = Math.floor((targetDate % (1000 * 60 * 60) /(1000 * 60)) );
     let secondsRemaining = Math.floor((targetDate % (1000 * 60) / 1000 ));
-    timeContainer.innerHTML = daysRemaining + " Days " + hoursRemaining + " Hours " + minutesRemaining + " Minutes " + secondsRemaining + " Seconds "
+    daysContainer.textContent = `${daysRemaining} Days`;
+    hoursContainer.textContent = `  ${hoursRemaining} Hours  `;
+    minutesContainer.textContent= `  ${minutesRemaining} Minutes  `;  
+    secondsContainer.textContent = `  ${secondsRemaining} Seconds  `;
     if (targetDate < 0){
         timeContainer.innerHTML = "Your staking period has ended, please go to the staking website and claim your tokens or restake!"
         clearInterval(countdownInterval)
